b919d26b432dc13ecc6498f5d30bb65af23416a6
Giovannincs <giovannincs@hotmail.com>
2024-02-09 22:22:14 +0000

    feat:implementing validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,6 +1,15 @@
 from src.views.http_types.http_response import HttpResponse
-
+from src.errors.error_types.http_umprocessable_entity import HttpUmprocessableEntityError
 def handler_errors(error:Exception)-> HttpResponse:
+    if isinstance(error,HttpUmprocessableEntityError):
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+                'errors':[{
+                'title':error.name,
+                "detail":error.message}]
+            }
+        )
     return HttpResponse(
         status_code=500,
         body={
